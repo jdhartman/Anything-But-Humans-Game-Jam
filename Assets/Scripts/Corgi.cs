9b3e083c546f8ed7db6d2dc9a996d4e4ae27ce12
Justin Hartman <hartma62@purdue.edu>
2017-01-02 22:33:46 -0500

    Setting Up Corgi Animation
    ...

---
+++ Assets/Scripts/Corgi.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corgi : MonoBehaviour {
+
+
+    public float speed;
+    private Rigidbody2D rb2d;
+    //private Vector2 move;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical"));
+        rb2d.velocity = move;
+	}
+}
