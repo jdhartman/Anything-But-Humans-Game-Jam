bd9878c582afd940f07397096bf2d07cdc3f61a9
Justin Hartman <hartma62@purdue.edu>
2017-01-06 13:45:16 -0500

    Added Target for Objective
    Create multiple targets, get their locations at the start of the game,
    and let those be the respawn points for the objective.

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+}
