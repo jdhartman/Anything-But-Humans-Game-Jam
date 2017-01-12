670e2e786959c3bf8260c707da8cc4cb653c400f
Justin Hartman <hartma62@purdue.edu>
2017-01-11 21:24:46 -0500

    Added collisions wall and working Camera movement
    -need to work on ui and timer system.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public float maxX;
+    public float maxY;
+    public float minX;
+    public float minY;
+
+    public Transform target;
+    public Vector3 Camera;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(target != null)
+        {
+            transform.position = new Vector3(Mathf.Clamp(target.position.x, minX, maxX), Mathf.Clamp(target.position.y, minY, maxY), target.position.z - 800);
+
+        }
+    }
+}
