d23afb0254c1384eacb225fc027e73669af32247
Justin Hartman <hartma62@purdue.edu>
2017-01-16 15:20:48 -0500

    Added Tree Tops, Working Depth Controller, and new font
    Still need:
    - more scenery
    - highscore keeper
    - slow motion

--- Assets/Scripts/DepthController.cs
+++ Assets/Scripts/DepthController.cs
@@ -5,14 +5,20 @@ using System.Collections;
 public class DepthController : MonoBehaviour {
 
     public Renderer cachedSpriteRenderer;
+    public bool isMoving;
+    public int offset;
 
     private void Start()
     {
         cachedSpriteRenderer = GetComponent<Renderer>();
+        cachedSpriteRenderer.sortingOrder = (int)(transform.position.y * -10) + offset;
     }
 
     void Update()
     {
-        cachedSpriteRenderer.sortingOrder = (int)(transform.position.y * -10);
+        if(isMoving)
+        {
+            cachedSpriteRenderer.sortingOrder = (int)(transform.position.y * -10);
+        }
     }
 }
