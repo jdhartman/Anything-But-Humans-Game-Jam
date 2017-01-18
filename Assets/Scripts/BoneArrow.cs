b95837fc4ba6a10cdb3d852415d959a8b9d446f8
Justin Hartman <hartma62@purdue.edu>
2017-01-18 00:55:32 -0500

    Working Rotating Arrow
    -highscore
    -more scenery
    -slow motion ending

---
+++ Assets/Scripts/BoneArrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneArrow : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject bone;
+    public GameObject arrow;
+    public SpriteRenderer renderer;
+    private Vector3 rotationMask;
+    private Vector3 difference;
+    private float angle;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if(!renderer.isVisible)
+        {
+            arrow.layer = 0;
+            renderer.sortingLayerName = "Default";
+            difference = transform.position - bone.transform.position;
+            angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg + 90;
+            Debug.Log(angle);
+            //Vector3 newDir = Vector3.RotateTowards(transform.position, difference, 60, 200);
+            //transform.Rotate(Vector3.forward, angle / Time.deltaTime, Space.Self);
+            transform.rotation = Quaternion.AngleAxis(angle, transform.forward);
+        }
+        else
+        {
+            arrow.layer = 8;
+        }
+        
+    }
+}
