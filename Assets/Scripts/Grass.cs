41fb00cbd24453c04777d6a5fc0ee0081ac44fdf
Justin Hartman <hartma62@purdue.edu>
2017-01-27 22:12:46 -0500

    Removed Build

--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -6,9 +6,11 @@ public class Grass : MonoBehaviour {
 
     public float animSpeed;
     private Animator anim;
+    private AudioSource walk;
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        walk = GetComponent<AudioSource>();
         anim.speed = 0;
 	}
 
@@ -17,6 +19,11 @@ public class Grass : MonoBehaviour {
         if(collision.gameObject.tag == "Player" )
         {
             anim.speed = animSpeed;
+            if(!walk.isPlaying)
+            {
+                walk.Play();
+            }
+
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -24,6 +31,7 @@ public class Grass : MonoBehaviour {
         if (collision.gameObject.tag == "Player")
         {
             anim.speed = 0;
+            walk.Stop();
         }
     }
 }
